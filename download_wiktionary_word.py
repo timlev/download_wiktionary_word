85a86aa877d2aff9abe2a4ac9f728bcd47e7e94b
Tim Leverentz <timlev@gmail.com>
2019-01-21 10:07:30 -0600

    first commit

---
+++ download_wiktionary_word.py
@@ -0,0 +1,137 @@
+import urllib2
+import os
+import tempfile
+import platform
+import bs4
+
+
+def check_downloaded_word(word, directory="./"):
+    soundfiles = os.listdir(directory)
+    #strip extension
+    downloaded_words = [os.path.splitext(x)[0] for x in soundfiles]
+    if word in downloaded_words:
+        return True
+    else:
+        return False
+
+def get_wiki(word, directory="./"):
+    if check_downloaded_word(word, directory):
+        return 0
+    #search for wiktionary word
+    base = "https://en.wiktionary.org/wiki/"
+    query = base + urllib2.quote(word)
+    print query
+    try:
+        response = urllib2.urlopen(query)
+    except:
+        print "Couldn't find", word
+        return 1
+    print "Processing response"
+    index = bs4.BeautifulSoup(response,"html5lib")
+    filenameguess = "File:en-us-" + word + ".ogg"
+    #print index.find(title=filenameguess)
+    #Jump to file wiktionary page
+    query = base + filenameguess
+    response = urllib2.urlopen(query)
+    index = bs4.BeautifulSoup(response, "html5lib")
+    links = index.find_all("a")
+    oggsource = ""
+    for link in links:
+        href = str(link.get("href"))
+        if "upload" in href and "n-us" in href and ".ogg" in href and word in href:
+            oggsource = "https:" + href
+
+    print "Downloading to: " + os.path.join(directory, word + ".ogg")
+    try:
+        print "Getting ogg..."
+        getogg = urllib2.urlopen(oggsource)
+        print "Saving file ..."
+        ofp = open(os.path.join(directory, word + ".ogg"),'wb')
+        print "Writing file ..."
+        ofp.write(getogg.read())
+        ofp.close()
+        return os.path.join(directory, word + ".ogg")
+    except:
+        #print "Could not download:", word
+        return 2
+
+    # oggsource = ""
+    # for line in response:
+        # if "src" in line and "n-us" in line and ".ogg" in line:
+            # print line
+            # start = line.find("""src="//""") + len("""src="//""")
+            # end = line.find(".ogg") + len(".ogg")
+            # oggsource = line[start:end]
+            # oggsource = "https://" + oggsource
+            # print oggsource
+            # break
+    # print query
+    # print oggsource
+    # print "Downloading to:", os.path.join(directory, word + ".ogg")
+    # try:
+        # print "Getting ogg..."
+        # getogg = urllib2.urlopen(oggsource)
+        # print "Saving file ..."
+        # ofp = open(os.path.join(directory, word + ".ogg"),'wb')
+        # print "Writing file ..."
+        # ofp.write(getogg.read())
+        # ofp.close()
+        # return os.path.join(directory, word + ".ogg")
+    # except:
+        # #print "Could not download:", word
+        # return 2
+        
+#download wiktionary ogg file
+
+def download_gstatic(word, directory="./"):
+    if check_downloaded_word(word, directory):
+        return 0
+    base = "https://ssl.gstatic.com/dictionary/static/sounds/de/0/"
+    query = base + word + ".mp3"
+    print query
+    try:
+        response = urllib2.urlopen(query)
+    except:
+        print "Couldn't find", word
+        return 1
+    try:
+        print "Getting mp3..."
+        getmp3 = urllib2.urlopen(query)
+        print "Saving file ..."
+        ofp = open(os.path.join(directory, word + ".mp3"),'wb')
+        print "Writing file ..."
+        ofp.write(getmp3.read())
+        ofp.close()
+        return os.path.join(directory, word + ".mp3")
+    except:
+        #print "Could not download:", word
+        return 2
+
+#convert ogg to mp3
+
+def convert_ogg_to_mp3(oggfile, remove_ogg = False):
+    oggpath = os.path.abspath(oggfile)
+    ogg_dir = os.path.dirname(oggfile)
+    oggfile = os.path.basename(oggfile)
+    mp3file = oggfile.replace(".ogg", ".mp3")
+    mp3path = oggpath.replace(".ogg",".mp3")
+    os.system('ffmpeg -i "' + oggpath + '" -acodec libmp3lame "' + mp3path + '"')
+    if remove_ogg:
+        os.remove(oggpath)
+    return mp3path
+
+if __name__ == "__main__":
+    #if get_wiki("joyful") == 0:
+    #    convert_ogg_to_mp3("i'm" + ".ogg", True)
+    #print download_gstatic("blowhole")
+    #print download_gstatic("myword")
+    #wordlist = ["zero", "ten", "twenty", "one", "eleven", "twenty-one", "two", "twelve", "twenty-two", "three", "thirteen", "twenty-three", "four", "fourteen", "twenty-four", "five", "fifteen", "twenty-five", "six", "sixteen", "twenty-six", "seven", "seventeen", "twenty-seven", "eight", "eighteen", "twenty-eight", "nine", "nineteen", "twenty-nine", "thirty", "forty", "seventy", "thirty-one", "fifty", "eighty", "thirty-two", "sixty", "ninety"]
+    wordlist = ["musician"]
+    print len(wordlist)
+    for word in wordlist:
+        get_wiki(word)
+        """try:
+            convert_ogg_to_mp3(word + ".ogg", True)
+        except:
+            print "************\n Problem with " + word + "\n******************\n"
+"""
---
+++ record_missing_words.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import os, argparse, sys, platform
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", nargs='+')
+parser.add_argument("--output_dir", default="./sounds")
+#parser.add_argument("--skip_sounds", action="store_true")
+args = parser.parse_args(sys.argv[1:])
+
+print args.input
+
+def record(word, directory = args.output_dir):
+    os.system('rec "' + os.path.join(directory, word) + '.wav"')
+def convert(word, directory = "./"):
+    wavpath = os.path.join(directory, word) + '.wav'
+    mp3path = os.path.join(directory, word) + '.mp3'
+    #if platform.system() == 'Linux':
+        #os.system('avconv -i "' + wavpath + '" "' + mp3path + '"')
+   # else:
+    os.system('ffmpeg -i "' + wavpath + '" -acodec libmp3lame "' + mp3path + '"')
+
+def remove_wave(filename, directory = "./"):
+    os.remove(os.path.join(directory, filename))
+    print "rm " + filename
+
+
+#filename = "missing_words.txt"
+#filename = "missing_words/The One That Got Away.html_missing_words.txt"
+#filename = "missing_words/Jumping In.html_missing_words.txt"
+#filename = "missing_words/George Washington Carver.html_missing_words.txt"
+#filename = "missing_words/Early Birds.html_missing_words.txt"
+#filename = "missing_words/The One That Got Away.html_missing_words.txt"
+allfilenames = args.input
+for filename in allfilenames:
+    #soundsdir = "/home/levtim/GitProjects/read-story/sounds"
+    #soundsdir = "./sounds"
+    soundsdir = args.output_dir
+
+    soundlist = os.listdir(soundsdir)
+
+
+    soundlist = [x.replace(".mp3","") for x in soundlist if not x.startswith(".")]
+
+    wordlist = []
+
+    with open(filename, "rb") as fp:
+        wordlist = fp.readlines()
+
+    wordlist = [x.rstrip() for x in wordlist if not x.startswith(".")]
+
+
+    missing_words = [x for x in wordlist if x not in soundlist]
+
+    print missing_words
+
+    for word in missing_words:
+        print "\n\n\n\n\n"
+        print word
+        print "**********************************************"
+        print "\n\n\n\n\n"
+        record(word, soundsdir)
+        #record(word)
+
+
+    for word in missing_words:
+        convert(word, soundsdir)
+        #convert(word)
+
+    for word in missing_words:
+        remove_wave(word + ".wav", soundsdir)
+        #remove_wave(os.path.join(soundsdir, word) + ".wav")
